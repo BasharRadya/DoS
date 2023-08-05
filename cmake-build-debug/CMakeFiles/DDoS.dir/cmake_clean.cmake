cd85eef2965a9cffc76a23d3f5413224b082c278
basharradya <basharabdelgafer@gmail.com>
2023-08-05 20:45:07 +0300

    Dddoser

---
+++ cmake-build-debug/CMakeFiles/DDoS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DDoS.dir/Ddoser.cpp.o"
+  "DDoS.pdb"
+  "DDoS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DDoS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
